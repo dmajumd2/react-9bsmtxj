9aac0bc700ac61b84dc232ad5482f79dc26dbdd5
dmajumd2 <debashish271991@gmail.com>
2026-01-03 23:04:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-9bsmtxj
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/dmajumd2/react-9bsmtxj)
---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
+export default function app() {
+  //const counter= 0;
+  const [counter, setCounter] = useState(0);
+  
+  return (
+    <>
+      <p>Counter is {counter}</p>
+      <button onClick={() => setCounter(counter + 1)}> + </button>
+      <p></p>
+      <button onClick={() => setCounter(c => (c > 0 ? c - 1 : 0))}> - </button>
+    </>
+  );
+}
+
+export default function User() {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    //async function fetchUsers(){
+      fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => {
+        let v = res.json();
+        return v
+      })
+      .then((data) => {
+        setUsers(data)
+      })
+  },[])
+  return (
+   <>
+   <ul>
+     {users.map(u => (
+       <li key={u.id}>{u.name}</li>
+     ))}
+    </ul>
+   </>
+  );
+}
